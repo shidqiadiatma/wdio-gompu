afb522ab6cc6a6bfa73d0c2e092df560200f224f
hipstertester <shidqiadiatma12@gmail.com>
2023-04-03 13:03:47 +0700

    fix error

--- wdio.conf.js
+++ wdio.conf.js
@@ -134,8 +134,7 @@ exports.config = {
     // Test reporter for stdout.
     // The only one supported by default is 'dot'
     // see also: https://webdriver.io/docs/dot-reporter
-    reporters: ['spec'],
-    reporters: [['allure', {
+    reporters: ['spec'] [['allure', {
         outputDir: 'allure-results',
         disableWebdriverStepsReporting: true,
         disableWebdriverScreenshotsReporting: true,
