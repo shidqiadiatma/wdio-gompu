0dde489382f74f1248532144763baaf326f1d590
hipstertester <shidqiadiatma12@gmail.com>
2023-04-06 15:43:56 +0700

    refactor code

--- test/pageobjects/forgotPassword-page.js
+++ test/pageobjects/forgotPassword-page.js
@@ -1,11 +1,6 @@
 const Page = require('./page');
-/**
- * sub page containing specific selectors and methods for a specific page
- */
+
 class forgotPasswordPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
     get btnloginPage () {
         return $('//*[@id="__layout"]/div/div/header/div[1]/div[2]/a');
     }
@@ -18,10 +13,6 @@ class forgotPasswordPage extends Page {
     get button_submit () {
         return $('button[type="submit"]');
     }
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
     async go_to_forgotPasswordPage(){
         await this.btnloginPage.click()
         await this.btnForgotpassword.click()
@@ -31,11 +22,9 @@ class forgotPasswordPage extends Page {
         await this.inputEmail.setValue(email);
         await this.button_submit.click();
     }
-    /**
-     * overwrite specific options to adapt it to page object
-     */
     open () {
         return super.open();
     }
 }
 module.exports = new forgotPasswordPage();
+
